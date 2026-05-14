9b5a61122ea70c2bcc8897492c6726b846848b33
Aoanul Hoque <aoanulh2789@nycstudents.net>
2026-05-13 22:23:05 -0400

    grabbing alerts  and printing them  (playing around with fetching)

--- app/test.py
+++ app/test.py
@@ -4,13 +4,30 @@ from pprint import pprint
 
 URL="https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/camsys%2Fsubway-alerts.json"
 
-def call(n):
+def call():
     global URL
     with urllib.request.urlopen(URL) as response:
         raw_data = response.read()
     data = json.loads(raw_data)
     meat = data['entity']
+    cleaned_meat = []
     print(len(meat))
-    return meat[n]
-
-pprint(call(3))
+    for item in meat:
+        alert = item["alert"]
+        title =  alert["header_text"]["translation"][0]["text"]
+        desc = alert.get("description_text", {
+            "translation": [{"text": "There's no damn description"}]
+        })["translation"][0]["text"]
+        trains = []
+        for i in alert["informed_entity"]:
+            if "route_id" in i:
+                trains.append(i["route_id"])
+        cleaned_alert = {
+            "id": item["id"],
+            "trains": trains,
+            "title":  title,
+            "desc": desc
+        }
+        cleaned_meat.append(cleaned_alert)
+    return  cleaned_meat
+pprint(call())
